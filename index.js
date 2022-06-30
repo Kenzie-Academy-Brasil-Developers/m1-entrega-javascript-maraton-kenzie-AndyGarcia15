35b17cf4f5dd11f76fc9783a9d4900f98b00e0af
Andy Garcia <102547835+AndyGarcia15@users.noreply.github.com>
2022-06-30 11:32:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ function positions(array){
         for(let i = 0; i < array.length;i++){
             if(array[i] == 'Daniel'){
                 substituicao = array.splice(i-1,0,array.splice(i,1)[0]);
-                
+                return console.log('Este é o pódio ' + array)
             }
         }
     }
-    return console.log(array)
+    else{
+        return console.log("Daniel é o vencedor")
+    }
 }
 positions(competidores);
 
